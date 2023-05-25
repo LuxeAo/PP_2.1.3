e290ed38c02cf290127ec2a5ea397357c7ac599d
LuxeAo <121669318+LuxeAo@users.noreply.github.com>
2023-05-25 23:06:35 +0400

    Test Pass

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean==bean2);
+        Cat catbean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean1.getName());
+        Cat catbean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean2.getName());
+        System.out.println(catbean1==catbean2);
     }
 }
